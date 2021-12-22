8308c39f9125955ba914c2e267730873ab00af82
Ruslan Usmanov <ruslanusm6@gmail.com>
2021-12-22 20:21:08 +0500

    Initial Commit

---
+++ hello-world/hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_worldApp.swift
+//  hello-world
+//
+//  Created by Ruslan Usmanov on 22.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
